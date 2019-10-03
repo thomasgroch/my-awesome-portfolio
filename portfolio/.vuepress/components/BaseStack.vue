8772244b079978a3cf8b794a11b547995b32efd9
Thomas Groch <thomas.groch@gmail.com>
2019-10-03 20:59:49 -0300

    forntmatter fix, first commit for baseStack

--- portfolio/.vuepress/components/BaseStack.vue
+++ portfolio/.vuepress/components/BaseStack.vue
@@ -1,31 +1,41 @@
 <template>
-    <div class="mt-5 flex flex-col items-center">
-        <ul class="flex mb-4 justify-center list-reset">
-            <li class="inline-block text-center border border-green rounded py-2 px-4 mr-4 text-white"
-                v-for="stack in items">
-                <Stack :item="stack"></Stack>
-            </li>
-        </ul>
-    </div>
+ <div class="m-auto md:w-1/2 lg:w-1/2">
+    <TopMenu></TopMenu>
+    <h1 class="text-center text-2xl text-orange-500 py-3">Stack</h1>
+
+    <p class="text-center text-gray-700 px-10 py-10 content">
+      
+     <span class="text-lg">{{stack.description}}</span> 
+
+      <div class="flex w-full flex-wrap mb-10 justify-center">
+          <StackCard v-for="stack in stack.techs" 
+                    :item="stack"
+                    :key="stack.name"></StackCard>
+      </div>
+      
+    </p>
+
+    <Footer class="m-auto text-sm text-center"></Footer>
+</div>
 </template>
+
 <script>
-	import Stack from './Stack'
-	export default {
-		name: 'BaseStack',
-		components: {Stack},
-		props: {
-			'items': {
-				type: Array,
-                default: function () {
-                    return [{
-                    	name: '',
-						domain: ''
-                    }]
-				}
-			},
-            // 'greyscale'
-		}
-	}
+import StackCard from "@/components/cards/StackCard.vue";
+import TopMenu from "@/components/TopMenu.vue";
+import Footer from "@/components/Footer.vue";
+import info from "@/info.json";
+
+export default {
+  name: "StackPage",
+  data() {
+   return {
+      stack: info.stack
+    }
+  },
+  components: {
+    StackCard,
+    TopMenu,
+    Footer,
+  }
+}
 </script>
-<style lang="stylus">
-</style>
---
+++ portfolio/.vuepress/components/StackCard.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="mr-5">
+  <a :href="item.href"
+      target="_blank">
+    <img class="  shadow h-8 sm:h-16 rounded-full"
+        :src="item.photo" />
+  </a>
+
+  <div class="border-transparent hover:border-orange-500 border-2 rounded-full mt-2 mb-6  py-2 px-4  text-gray-700 text-md">
+    <a v-text="item.name"
+      :href="item.href"
+      target="_blank"></a>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Stack",
+
+  props: {
+    item: {
+      type: Object,
+      default: function() {
+        return {
+          name:'',
+          photo:'',
+          href:''
+        }
+      }
+    },
+  }
+};
+</script>
