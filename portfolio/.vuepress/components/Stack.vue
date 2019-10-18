44836fd53057d339561def40385d99b1161ec363
Thomas Groch <thomas.groch@gmail.com>
2019-10-18 17:00:45 -0300

    fix

--- portfolio/.vuepress/components/Stack.vue
+++ portfolio/.vuepress/components/Stack.vue
@@ -29,6 +29,9 @@
 		},
 		methods: {
 			getHostName(url) {
+				if (!url){
+					return null
+                }
 				const match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i)
 				if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
 					return match[2]
